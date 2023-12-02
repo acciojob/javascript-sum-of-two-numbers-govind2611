6833f77b3de147786b7fcc375d6b06cca51a297b
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-12-02 16:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let a = prompt("Enter first number");
 let b = prompt("Enter second number");
-
+// 
 	if(a!=undefined && b!=undefined)
          document.getElementById('output').innerText = parseInt(a)+parseInt(b);
 	else
